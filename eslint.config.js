ef96750b48abecdf085b59ac9dd6da9e69f36f86
rameez <rameezwang@gmail.com>
2026-05-09 21:18:51 +0530

    Stabilize Tina build on Vercel

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
-  { ignores: ["dist"] },
+  { ignores: ["dist", "public/admin", "tina/__generated__"] },
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
